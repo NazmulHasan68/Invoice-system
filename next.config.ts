4f10ea58652f771dad70c6dda191c5d3cf770d51
NazmulHasan68 <aishadowzone@gmail.com>
2025-11-17 13:17:15 +0600

    now fixed it

--- next.config.ts
+++ next.config.ts
@@ -1,34 +1,17 @@
-// import type { NextConfig } from "next";
-
-// const nextConfig: NextConfig = {
-//   /* config options here */
-//   reactCompiler: true,
-//   images: {
-//     domains: ["upload.wikimedia.org","res.cloudinary.com",], // ✅ এখানে তোমার ডোমেইন যুক্ত হলো
-//   },
-// };
-
-// export default nextConfig;
 
 
-// next.config.ts
-import { NextConfig } from 'next';
-
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   images: {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'res.cloudinary.com',
+        hostname: 'upload.wikimedia.org',
         pathname: '/**', // allow all paths
-      },
-      {
-        protocol: 'https',
-        hostname: 'example.com',
-        pathname: '/images/**', // allow only /images path
       },
     ],
   },
-};
+}
 
-export default nextConfig;
+module.exports = nextConfig;
+
