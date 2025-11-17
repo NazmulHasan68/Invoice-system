eb2cd5aee16e32284e5a15660d0331d204923c4a
NazmulHasan68 <aishadowzone@gmail.com>
2025-11-17 13:18:26 +0600

    again try

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,19 @@
-
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'upload.wikimedia.org',
+        hostname: 'res.cloudinary.com',
         pathname: '/**', // allow all paths
+      },
+      {
+        protocol: 'https',
+        hostname: 'upload.wikimedia.org',
+        pathname: '/**', // previously added
       },
     ],
   },
 }
 
 module.exports = nextConfig;
-
