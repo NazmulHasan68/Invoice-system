f746aaa311d5f0eda928570c16cec91c34bf36c6
NazmulHasan68 <aishadowzone@gmail.com>
2025-11-17 12:32:31 +0600

    add

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,33 @@
-import type { NextConfig } from "next";
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   /* config options here */
+//   reactCompiler: true,
+//   images: {
+//     domains: ["upload.wikimedia.org","res.cloudinary.com",], // ✅ এখানে তোমার ডোমেইন যুক্ত হলো
+//   },
+// };
+
+// export default nextConfig;
+
+
+// next.config.ts
+import { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  reactCompiler: true,
   images: {
-    domains: ["upload.wikimedia.org","res.cloudinary.com"], // ✅ এখানে তোমার ডোমেইন যুক্ত হলো
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        pathname: '/**', // allow all paths
+      },
+      {
+        protocol: 'https',
+        hostname: 'example.com',
+        pathname: '/images/**', // allow only /images path
+      },
+    ],
   },
 };
 
