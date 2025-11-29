2140dac5b35e2513f1cac43e9144ce1bc7e173c3
NazmulHasan68 <aishadowzone@gmail.com>
2025-11-29 22:13:22 +0600

    asdfsd

--- src/app/admin-dashboard/settings/page.tsx
+++ src/app/admin-dashboard/settings/page.tsx
@@ -49,7 +49,7 @@ export default async function Settings() {
 
       <Card>
         <CardHeader>
-          <CardTitle>Category Management </CardTitle>
+          <CardTitle>Category Management sdfsdf sd</CardTitle>
         </CardHeader>
         <CardContent>
           <CategoryManager categories={Array.isArray(categories) ? categories : []} />
