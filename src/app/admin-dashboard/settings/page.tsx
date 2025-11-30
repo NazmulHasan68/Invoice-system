af798f5653dd6f04611dadb41840b7db24d5aca3
NazmulHasan68 <aishadowzone@gmail.com>
2025-11-30 10:57:39 +0600

    fg

--- src/app/admin-dashboard/settings/page.tsx
+++ src/app/admin-dashboard/settings/page.tsx
@@ -49,7 +49,7 @@ export default async function Settings() {
 
       <Card>
         <CardHeader>
-          <CardTitle>Category Management ghfg </CardTitle>
+          <CardTitle>Category Management </CardTitle>
         </CardHeader>
         <CardContent>
           <CategoryManager categories={Array.isArray(categories) ? categories : []} />
